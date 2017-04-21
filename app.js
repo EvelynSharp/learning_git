8690b5c6e189ba6f43868be71fc5d625150118b8
Evelyn Sharp <yihongshi90@gmail.com>
2017-04-20 20:30:32 -0600

    age and if else

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ alert('welcome to Git!');
 console.log('we are on a new branch');
 var firstName  = 'Evelyn';
 var lastName = 'Sharp';
+var age = 27;
 console.log('Full Name: '+firstName+' '+lastName);
+if (age > 19) {
+  console.log("Not a Teen");
+} else {
+  console.log("Teen");
+}
